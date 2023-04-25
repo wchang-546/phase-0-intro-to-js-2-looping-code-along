4997729aaa6930ac606a45aa51c5e0dda2d33c14
Will Chang <william.chaang@gmail.com>
2023-04-25 12:53:39 -0600

    Looping lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, event){
+    const thankyouArray = [];
+    for (let i = 0; i < names.length; i++){
+        thankyouArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return(thankyouArray);
+}
+
+
+function countDown(number){
+    let countDown = number;
+    while (countDown >= 0){
+        console.log(countDown--);
+    }
+}
+
+
